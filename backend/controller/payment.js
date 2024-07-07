3132d31e7bf20e004ff34a057638248383c75df4
rahul-roshan-4 <101462374+rahul-roshan-4@users.noreply.github.com>
2024-07-07 10:52:01 +0530

    Update payment.js

--- backend/controller/payment.js
+++ backend/controller/payment.js
@@ -11,7 +11,7 @@ router.post(
       amount: req.body.amount,
       currency: "inr",
       metadata: {
-        company: "Becodemy",
+        company: "NIT Jamshedpur",
       },
     });
     res.status(200).json({
